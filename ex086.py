7f9a7cab4956d9d743f3cd215d3ae192f62cc319
Paulo Souza <paulosergio.games1997@gmail.com>
2022-04-29 23:06:14 -0300

    ...
    ao invés de adicionar valores o código vai randomizar valores

--- ex086.py
+++ ex086.py
@@ -1,7 +1,8 @@
+from random import randint
 matriz = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
 for lista in range(0, 3):
     for coluna in range(0, 3):
-        matriz[lista][coluna] = int(input(f'Digite um valor para [{lista}, {coluna}]: '))
+        matriz[lista][coluna] = randint(0, 99)
 print('=-=' * 15)
 for lista in range(0, 3):
     for coluna in range(0, 3):
