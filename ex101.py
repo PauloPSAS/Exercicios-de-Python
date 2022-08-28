6bc7e3b6cd0e5726382aef0d5aa49478e55bea49
Paulo Souza <77758720+PauloPSAS@users.noreply.github.com>
2022-08-27 23:04:17 -0300

    Update ex101.py
    pequena otimização do programa

--- ex101.py
+++ ex101.py
@@ -1,7 +1,5 @@
-from datetime import datetime
-
-
 def idade(n):
+    from datetime import datetime
     ano_atual = datetime.today().year
     idd = ano_atual - n
     return idd
@@ -10,7 +8,7 @@ def idade(n):
 def voto(a):
     if a <= 15:
         print('\033[1;31mNÃO VOTA.')
-    elif a <= 17 or a > 70:
+    elif a <= 17 or a > 65:
         print('\033[1;36mVOTO OPCIONAL.')
     else:
         print('\033[1;34mVOTO OBRIGATÓRIO.')
