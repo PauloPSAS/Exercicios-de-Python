76d81064ce3aadc2f95078fc2b9f73066a1aa5fe
Paulo Souza <77758720+PauloPSAS@users.noreply.github.com>
2022-08-29 14:30:16 -0300

    Update ex105.py

--- ex105.py
+++ ex105.py
@@ -1,4 +1,10 @@
 def notas(*n, sit=False):
+    """
+    => Função para analisar notas e situações de vários alunos.
+    :param n: Uma ou mais notas do aluno (aceita várias).
+    :param sit: Valor opcional, indica se deve ou não mostrar a situação do aluno.
+    :return: Dicionário com as informações do aluno.
+    """
     mai = men = med = 0
     dic = dict()
     dic['total'] = len(n)
@@ -26,4 +32,5 @@ def notas(*n, sit=False):
 
 # Main
 resp = notas(9, 9, 3, 8.4, sit=True)
+help(notas)
 print(resp)
