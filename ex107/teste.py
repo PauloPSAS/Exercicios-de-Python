f8d4573623d835c42d5a90dfd8ac76df66948f81
Paulo Souza <77758720+PauloPSAS@users.noreply.github.com>
2022-09-01 13:08:30 -0300

    alteração nas pastas
    alteração nas pastas e organização dos exercícios

--- ex107/teste.py
+++ ex107/teste.py
@@ -3,4 +3,4 @@ import moeda
 valor = float(input('Digite o preço: R$ '))
 print(f'A metade de R$ {valor:.2f} é R$ {moeda.metade(valor):.2f}')
 print(f'O dobro de R$ {valor:.2f} é R$ {moeda.dobro(valor):.2f}')
-print(f'Aumento de 10% de R$ {valor:.2f} é {moeda.aumento10(valor):.2f}')
+print(f'Aumento de 10% de R$ {valor:.2f} é {moeda.aumentar(valor, 10):.2f}')
