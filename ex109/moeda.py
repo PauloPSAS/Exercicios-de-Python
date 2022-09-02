f8bdbf49b149e63427f1e0d7eade15fa1a373bf5
Paulo Souza <77758720+PauloPSAS@users.noreply.github.com>
2022-09-01 21:07:09 -0300

    .

--- ex109/moeda.py
+++ ex109/moeda.py
@@ -1,4 +1,10 @@
 def metade(v=0, form=False):
+    """
+    → Mostra a metade de um determinado valor.
+    :param v: valor que será dividido pela metade.
+    :param form: Forma com a qual será exibido o valor.
+    :return: O resultado do valor e a forma com a qual será mostrada
+    """
     res = v / 2
     return res if not form else moeda(res)
 
@@ -18,5 +24,5 @@ def diminuir(v=0, t=0, form=False):
     return res if form is False else moeda(res)
 
 
-def moeda(v=0, m='R$ '):
+def moeda(v: object = 0, m='R$ '):
     return f'{m}{v:.2f}'.replace('.', ',')
