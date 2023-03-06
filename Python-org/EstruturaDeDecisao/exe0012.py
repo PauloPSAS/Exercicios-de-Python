2a1c86fc063506210b848418a2fa869268da67cf
Paulo Souza <paulosergio.games1997@gmail.com>
2023-03-06 13:46:36 -0300

    Update exe0012.py

--- Python-org/EstruturaDeDecisao/exe0012.py
+++ Python-org/EstruturaDeDecisao/exe0012.py
@@ -9,3 +9,38 @@ Salário Bruto até 2500 (inclusive) - desconto de 10%
 Salário Bruto acima de 2500 - desconto de 20%
 Imprima na tela as informações, dispostas conforme o exemplo abaixo. No exemplo o valor da hora é 5 e a quantidade
 de hora é 220."""
+
+hT = float(input("Digite quantas horas foram trabalhadas: (H) "))
+sH = float(input("Digite quanto você ganha por hora: R$ "))
+
+sB = hT * sH
+inss = sB / 100 * 10
+fgts = sB / 100 * 11
+
+if sB <= 900:
+    pD = 0
+    ir = 0
+
+elif sB <= 1500:
+    pD = 5
+    ir = sB / 100 * pD
+
+elif sB <= 2500:
+    pD = 10
+    ir = sB / 100 * pD
+
+else:
+    pD = 20
+    ir = sB / 100 * pD
+
+tD = ir + inss
+sL = sB - tD
+
+print("\033[35m==" * 25)
+print(f"\033[1;33m{'Salário Bruto ':<25} : {'R$'} {sB:>10.2f}")
+print(f"\033[1;31m{f'(-) IR ({pD}%)':<25} : {'R$'} {ir:>10.2f}")
+print(f"\033[1;31m{'(-) INSS ( 10 % )':<25} : {'R$'} {inss:>10.2f}")
+print(f"\033[1;32m{'FGTS (11%)':<25} : {'R$'} {fgts:>10.2f}")
+print(f"\033[1;31m{'Total de descontos':<25} : {'R$'} {tD:>10.2f}")
+print(f"\033[1;34m{'Salário Liquido':<25} : {'R$'} {sL:>10.2f}")
+print("\033[35m==" * 25)
