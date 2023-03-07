0829abaf45e27ea526a1302f69773f122e7f3047
Paulo Souza <paulosergio.games1997@gmail.com>
2023-03-06 21:41:50 -0300

    Update exe0014.py

--- Python-org/EstruturaDeDecisao/exe0014.py
+++ Python-org/EstruturaDeDecisao/exe0014.py
@@ -11,15 +11,24 @@ Entre 4,0 e zero ----------E
 n1 = float(input("Primeira nota: "))
 n2 = float(input("Segunda nota: "))
 m = (n1 + n2) / 2
+s = ''
+c = ''
 
-print(f"Sua média de aproveitamento é: {m:.1f}", end=' ')
 if 0 <= m <= 4:
-    print("E")
+    c = 'REPROVADO'
+    s = 'E'
 elif 4 < m <= 6:
-    print("D")
+    c = 'REPROVADO'
+    s = 'D'
 elif 6 < m <= 7.5:
-    print("C")
+    c = 'APROVADO'
+    s = 'C'
 elif 7.5 < m <= 9:
-    print("B")
+    c = 'APROVADO'
+    s = 'B'
 else:
-    print("A")
+    c = 'APROVADO'
+    s = 'A'
+
+print("{:<23} {:>9}".format("Média de Aproveitamento", "Conceito"))
+print("{:>7.1f} {:>9} {:>16}".format(m, s, c))
