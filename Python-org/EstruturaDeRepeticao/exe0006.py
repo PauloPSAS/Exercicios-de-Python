98b8901c10df27a1d5058e6d1709ffe276b69fb7
paulo <paulosergio.games1997@gmail.com>
2023-04-10 22:58:29 -0300

    Create exe0006.py

---
+++ Python-org/EstruturaDeRepeticao/exe0006.py
@@ -0,0 +1,9 @@
+"""Faça um programa que imprima na tela os números de 1 a 20, um abaixo do outro. Depois modifique o programa para
+que ele mostre os números um ao lado do outro."""
+print("==" * 25)
+for i in range(1, 21):
+    print(i)
+print("==" * 25)
+for i in range(1, 21):
+    print(i, end=' ')
+print('\n'+"==" * 25)
