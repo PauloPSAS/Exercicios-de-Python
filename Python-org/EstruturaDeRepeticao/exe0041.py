0cd25916356afc979f93c86c77c4b692761fe9aa
paulo <paulosergio.games1997@gmail.com>
2023-07-03 22:07:08 -0300

    Create exe0041.py

---
+++ Python-org/EstruturaDeRepeticao/exe0041.py
@@ -0,0 +1,41 @@
+"""Faça um programa que receba o valor de uma dívida e mostre uma tabela com os seguintes dados:
+valor da dívida,
+valor dos juros,
+quantidade de parcelas e valor da parcela.
+
+Quantidade de Parcelas % de Juros sobre o valor inicial da dívida
+    1       0
+    3       10
+    6       15
+    9       20
+    12      25"""
+
+
+def parcela_invalida():
+    print("\nDigite um valor de parcelas válidos entre 1 e 12.")
+
+
+pct = valorParcelas = valorJuros = dividaFinal = 0
+divida = float(input("Digite o valor da divida: R$ "))
+while True:
+    parcelas = int(input("\nDigite em quantas parcelas será dividido a divida (entre 1 e 12): "))
+    if parcelas <= 0 or parcelas > 12:
+        parcela_invalida()
+    else:
+        break
+if 12 >= parcelas > 9:
+    pct = 25
+elif 9 >= parcelas > 6:
+    pct = 20
+elif 6 >= parcelas > 3:
+    pct = 10
+else:
+    pct = 0
+
+valorJuros = divida / 100 * pct
+dividaFinal = divida + valorJuros
+valorParcelas = dividaFinal / parcelas
+
+print(f"\nValor da dívida: R$ {divida:.2f}")
+print(f"Valor de {pct}% de juros sobre as {parcelas} parcela(s): R$ {valorJuros:.2f}")
+print(f"Valor final é de R$ {dividaFinal:.2f} divido em {parcelas} parcelas de R$ {valorParcelas:.2f}.")
